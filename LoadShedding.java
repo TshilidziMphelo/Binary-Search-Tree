c310da84d92173a4bca10a8402b31ba43b7f714b
Tshilidzi Mphelo <temphelo95@gmail.com>
2020-02-26 20:59:01 +0200

    Adding a compareTo and toString method to Loadshedding

--- LoadShedding.java
+++ LoadShedding.java
@@ -36,4 +36,75 @@ public class LoadShedding {
         // returns the area of the loadshedding object
         return area;
     }
+
+    /**
+     * compares two given loadsheding objects to see if they are equal or not
+     * does this by comparing their periods and areas
+     * returns true if they are and false if they are not
+     * 
+     * @param load this has a loadshedding object
+     */
+
+    /**
+     * compares loadshedding objects to see if the are equal or not
+     * this is done by comparing the periods and areas
+     * returns 0 if they are equal, 1 if the calling object is greater than given
+     * load shedding object
+     * and -1 if the calling object is less than the given object
+     * 
+     * @param other this has to be another loadsheddin object
+     */
+    public int compareTo(LoadShedding other) {
+        /*
+         * compares loadshedding objects to see if the are equal or not
+         * this is done by comparing the periods and areas
+         * returns 0 if they are equal, 1 if the calling object is greater than given
+         * load shedding object
+         * and -1 if the calling object is less than the given object
+         */
+        String p1 = this.period;
+        String p2 = other.getPeriod();
+
+        if (p1.substring(0, 1).compareTo(p2.substring(0, 1)) > 0) {
+            return 1;
+        } else if (p1.substring(0, 1).compareTo(p2.substring(0, 1)) < 0) {
+            return -1;
+        } else {
+            p1 = p1.substring(2); // updating p1 and p2 to compare the dates
+            p2 = p2.substring(2);
+
+            if (p1.substring(0, p1.indexOf("_")).compareTo(p2.substring(0, p2.indexOf("_"))) > 0) {
+                return 1;
+            } else if (p1.substring(0, p1.indexOf("_")).compareTo(p2.substring(0, p2.indexOf("_"))) < 0) {
+                return -1;
+            } else {
+                p1 = p1.substring(p1.indexOf("_") + 1); // updating p1 and p2 to compare the startTimes
+                p2 = p2.substring(p2.indexOf("_") + 1);
+
+                if (p1.compareTo(p2) > 0) {
+                    return 1;
+                } else if (p1.compareTo(p2) < 0) {
+                    return -1;
+                }
+                return 0;
+            }
+
+        }
+    }
+
+    /**
+     * gives you a string version of loadshedding
+     * 
+     * @return the loadSHedding object
+     */
+    public String toString() {
+        // gives you a string version of loadshedding
+        return period + area;
+    }
+
+    public static void main(String[] args) {
+        LoadShedding l = new LoadShedding("8_87_09", "7");
+        LoadShedding n = new LoadShedding("7_87_09", "7");
+        System.out.print(n.compareTo(l));
+    }
 }
