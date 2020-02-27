5ab8b1ebc23e4a81f82ea99ed7af36a1dac4ac21
Tshilidzi Mphelo <temphelo95@gmail.com>
2020-02-27 17:46:49 +0200

    Adding Binary Tree functions to LSBSTApp and finishing it

--- LSBSTApp.java
+++ LSBSTApp.java
@@ -39,4 +39,116 @@ public class LSBSTApp extends BinaryTree {
 
         return this;
     }
+
+    /**
+     * gives you the node of a certain period using the find method
+     * 
+     * @param stage String the stage of loadshedding
+     * @param date  String the date at which it is going to occur
+     * @param time  String the time at whic it is going to start
+     */
+    public BinaryTreeNode printAreas(String stage, String day, String time) {
+        String period = stage + "_" + day + "_" + time;
+        LoadShedding load = new LoadShedding(period, null);
+        // System.out.println(this.getBST().count());
+        return this.getBST().find(load);
+    }
+
+    /**
+     * prints out the entire binary search tree inorder
+     *
+     */
+    public void printAllAreas() {
+        this.getBST().inOrder();
+    }
+
+    /**
+     * returns the count of the number of comparisons
+     * 
+     * @return size of count
+     */
+    public int getCount() {
+        return i;
+    }
+
+    public void insert(LoadShedding d) {
+        if (root == null)
+            root = new BinaryTreeNode(d, null, null);
+        else
+            insert(d, root);
+    }
+
+    public void insert(LoadShedding d, BinaryTreeNode node) {
+        if (d.compareTo(node.data) <= 0) {
+            if (node.left == null)
+                node.left = new BinaryTreeNode(d, null, null);
+            else
+                insert(d, node.left);
+        } else {
+            if (node.right == null)
+                node.right = new BinaryTreeNode(d, null, null);
+            else
+                insert(d, node.right);
+        }
+    }
+
+    public BinaryTreeNode find(LoadShedding d) {
+        if (root == null)
+            return null;
+        else
+            return find(d, root);
+    }
+
+    public BinaryTreeNode find(LoadShedding d, BinaryTreeNode node) {
+        if (d.compareTo(node.data) == 0) {
+            i++;
+            return node;
+        } else if (d.compareTo(node.data) < 0) {
+            i++;
+            return (node.left == null) ? null : find(d, node.left);
+        } else {
+            i++;
+            return (node.right == null) ? null : find(d, node.right);
+        }
+    }
+
+    public void delete(LoadShedding d) {
+        root = delete(d, root);
+    }
+
+    public BinaryTreeNode delete(LoadShedding d, BinaryTreeNode node) {
+        if (node == null)
+            return null;
+        if (d.compareTo(node.data) < 0)
+            node.left = delete(d, node.left);
+        else if (d.compareTo(node.data) > 0)
+            node.right = delete(d, node.right);
+        else if (node.left != null && node.right != null) {
+            node.data = findMin(node.right).data;
+            node.right = removeMin(node.right);
+        } else {
+            if (node.left != null)
+                node = node.left;
+            else
+                node = node.right;
+        }
+        return node;
+    }
+
+    public BinaryTreeNode findMin(BinaryTreeNode node) {
+        if (node != null)
+            while (node.left != null)
+                node = node.left;
+        return node;
+    }
+
+    public BinaryTreeNode removeMin(BinaryTreeNode node) {
+        if (node == null)
+            return null;
+        else if (node.left != null) {
+            node.left = removeMin(node.left);
+            return node;
+        } else
+            return node.right;
+    }
 }
